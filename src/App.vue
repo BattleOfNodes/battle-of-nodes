38402cdadf3fb8e7c3bef9de62313fe9cd6118db
p0ulpe <delanoe.william@gmail.com>
2022-02-16 00:08:18 +0100

    Footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
     <div class="cntnt" id="app">
         <Header></Header>
         <router-view />
+        <Footer></Footer>
     </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default {
     components: {
         Header,
+        Footer
     },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="footer">
+        <img
+            class="footer__img"
+            alt="Battle of Nodes"
+            src="@/assets/images/battle-of-nodes-logo.png">
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

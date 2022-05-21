549fc3955e920ea0b6224048b4ee3ff28227b772
Andrei-George Caloian <48419225+caloianandreig@users.noreply.github.com>
2022-05-21 12:34:11 +0300

    menu-done

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,18 +1,30 @@
 <template>
     <nav class="menu">
-        <ul class="menu__links">
-            <li class="menu__item">
-                <router-link class="menu__item-link" to="/">About the game</router-link>
-            </li>
-            <li class="menu__item">
-                <router-link class="menu__item-link" to="/explore">Explore</router-link>
-            </li>
-            <li class="menu__item">
-                <router-link ref="fetch" class="menu__item-link" to="/my-collection">My Collection</router-link>
-            </li>
-            <li class="menu__item">
-                <router-link class="menu__item-link" to="/booster-pack">Booster Pack</router-link>
-            </li>
-        </ul>
+        <div class="menu__phone">
+            <input type="checkbox" class="menu__phone-check" />
+            <span class="menu__phone-span"></span>
+            <span class="menu__phone-span"></span>
+            <span class="menu__phone-span"></span>
+            <ul class="menu__links">
+                <li class="menu__item">
+                    <router-link class="menu__item-link" to="/">About the game</router-link>
+                </li>
+                <li class="menu__item">
+                    <router-link class="menu__item-link" to="/explore">Explore</router-link>
+                </li>
+                <li class="menu__item">
+                    <router-link ref="fetch" class="menu__item-link" to="/my-collection">My Collection</router-link>
+                </li>
+                <li class="menu__item">
+                    <router-link class="menu__item-link" to="/booster-pack">Booster Pack</router-link>
+                </li>
+                <li class="menu__item connectPhone">
+                    <router-link class="menu__item-link" to="/login">
+                        <span v-if="this.$erd.logged">{{ $erd.obfuscatedWalletAddress }}</span>
+                        <span v-else>Connect</span>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>

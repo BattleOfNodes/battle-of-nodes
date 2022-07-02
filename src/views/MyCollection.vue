aaae8a50859f1ea496001d2efda7c2889291b42c
Andrei-George Caloian <48419225+caloianandreig@users.noreply.github.com>
2022-07-02 18:19:31 +0300

    bg-clean

--- src/views/MyCollection.vue
+++ src/views/MyCollection.vue
@@ -67,12 +67,12 @@
                 </div>  
             </div>
             <template v-if="isDesktop">
-                <img class="cntnt__home-card cntnt__home-card-1" src="@/assets/images/bg-card-blue.png" />
-                <img class="cntnt__home-card cntnt__home-card-2" src="@/assets/images/bg-card-red.png" />
-                <img class="cntnt__home-card cntnt__home-card-3" src="@/assets/images/bg-card-blue.png" />
-                <img class="cntnt__home-card cntnt__home-card-4" src="@/assets/images/bg-card-red.png" />
-                <img class="cntnt__home-card cntnt__home-card-5" src="@/assets/images/bg-card-blue.png" />
-                <img class="cntnt__home-card cntnt__home-card-6" src="@/assets/images/bg-card-red.png" />
+                <img class="cntnt__bgCard cntnt__bgCard-1" src="@/assets/images/bg-card-blue.png" />
+                <img class="cntnt__bgCard cntnt__bgCard-2" src="@/assets/images/bg-card-red.png" />
+                <img class="cntnt__bgCard cntnt__bgCard-3" src="@/assets/images/bg-card-blue.png" />
+                <img class="cntnt__bgCard cntnt__bgCard-4" src="@/assets/images/bg-card-red.png" />
+                <img class="cntnt__bgCard cntnt__bgCard-5" src="@/assets/images/bg-card-blue.png" />
+                <img class="cntnt__bgCard cntnt__bgCard-6" src="@/assets/images/bg-card-red.png" />
             </template>
         </main>
     </div>

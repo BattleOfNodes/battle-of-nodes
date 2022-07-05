e41dd8138e3ef7f8cdf0405fe5e00a2d2204ac4a
Andrei-George Caloian <48419225+caloianandreig@users.noreply.github.com>
2022-07-05 19:16:56 +0300

    text-without-images

--- src/views/DownloadDemo/DownloadDemo.vue
+++ src/views/DownloadDemo/DownloadDemo.vue
@@ -5,10 +5,10 @@
             <p class="cntnt-textTitle">Demo Event Prerequisites:</p>
             <p class="cntnt-text">⦁Windows </p>
             <p class="cntnt-text">⦁Own a Elrond Wallet</p>
-            <p class="cntnt-text">⦁Own a 1st edition booster pack</p>
+            <p class="cntnt-text">⦁Own a&nbsp;<a style="color:white;" href="https://www.google.com/" target="_blank">1st edition booster pack</a></p>
             <p class="cntnt-textTitle">HOW TO DOWNLOAD & PLAY</p>
             <p class="cntnt-textBold">STEP 1</p>
-            <p class="cntnt-text">&emsp; Press the DOWNLOAD DEMO link</p>
+            <p class="cntnt-text">&emsp; Press the&nbsp;<a style="color:white;" href="https://www.google.com/" target="_blank">DOWNLOAD DEMO link</a></p>
             <p class="cntnt-textBold">STEP 2</p>
             <p class="cntnt-text">&emsp; Extract the .RAR file on your computer</p>
             <p class="cntnt-textBold">STEP 3</p>
@@ -19,13 +19,27 @@
             <p class="cntnt-text">&emsp; Connect with your Elrond wallet using the 4 methods of connection (make sure to not click on the game window before successfully connecting, otherwise you will need to connect again)</p>
             <p class="cntnt-textBold">STEP 6</p>
             <p class="cntnt-text">&emsp; Start playing! Create a deck, search for an opponent, and duel!</p>
+            <a style="margin-top:1rem;margin-bottom:2rem;" href="https://deadrare.io/collection/BONPACKS-f0b549" target="_blank" class="cntnt-btn cntnt__intro-btn">DOWNLOAD DEMO</a>
             <p class="cntnt-text">For more information or help feel free to join our discord server! We will help and guide during the Demo event.</p>
         </div>
+        <template v-if="isDesktop">
+            <img class="cntnt__bgCard cntnt__bgCard-1" src="@/assets/images/bg-card-blue.png" />
+            <img class="cntnt__bgCard cntnt__bgCard-2" src="@/assets/images/bg-card-red.png" />
+            <img class="cntnt__bgCard cntnt__bgCard-3" src="@/assets/images/bg-card-blue.png" />
+            <img class="cntnt__bgCard cntnt__bgCard-4" src="@/assets/images/bg-card-red.png" />
+            <img class="cntnt__bgCard cntnt__bgCard-5" src="@/assets/images/bg-card-blue.png" />
+            <img class="cntnt__bgCard cntnt__bgCard-6" src="@/assets/images/bg-card-red.png" />
+        </template>
     </main>
 </template>
 
 <script>
 export default {
-    name: 'Download'
+    name: 'Download',
+    data () {
+        return {
+            isDesktop: window.innerWidth >= 1024
+        }
+    }
 }
 </script>

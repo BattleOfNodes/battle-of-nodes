5c5ebfbf9405ad3a08a6a5c4b63601a04fe9a3a2
Andrei-George Caloian <48419225+caloianandreig@users.noreply.github.com>
2022-08-05 16:28:29 +0300

    link-mint-working

--- src/components/home/HomeInfos.vue
+++ src/components/home/HomeInfos.vue
@@ -3,7 +3,7 @@
         <div class="blockinfos">
             <h2 class="cntnt__zone-title blockinfos__title">Battle <br>of nodes</h2>
             <p class="blockinfos__txt">Battle of Nodes is an original Trading Card Game powered by the Elrond Network. Assemble a deck of powerful creatures & spells and duel away in a competitive, fun & rewarding gameplay. Inspired by the genesis of the blockchain, you can collect cards from 4 different civilizations. In a war between machines and the underworld, you must choose which side to take.</p>
-            <a href="https://deadrare.io/collection/BONPACKS-f0b549" target="_blank" class="cntnt-btn">Buy a pack now</a>
+            <a :href="link" target="_blank" class="cntnt-btn">Buy a pack now</a>
             <div class="cntnt__infos-img cntnt__infos-img-1">
                 <img src="@/assets/images/cards/card2.jpg" />
             </div>
@@ -18,3 +18,77 @@
         </div>
     </section>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return{
+            link: 'https://deadrare.io/collection/BONPACKS-f0b549',
+
+            devApi : "https://api.elrond.com",
+            SCAddressStr: "erd1qqqqqqqqqqqqqpgqfj9qht90c9zldjskq62sfx8ugfdxpjte58sq7r8au4"
+        }
+    },
+    methods: {
+        async checkIfMintIsLive() {
+
+            let rawMintedPacks = await axios.post(`${this.devApi}/query`,
+                {
+                    "scAddress" : this.SCAddressStr,
+                    "funcName"  : "getTotalNFTBuyable",
+                    "args"      : ["00"],
+                    "value"     : "0"
+                }
+            );
+
+            let mintedPacks = Number("0x" + Buffer.from(rawMintedPacks.data.returnData[0], 'base64').toString("hex"));
+
+            let rawBoughtPacks = await axios.post(`${this.devApi}/query`, {
+                    "scAddress" : this.SCAddressStr,
+                    "funcName"  : "getNbrNFTBought",
+                    "args"      : ["00"],
+                    "value"     : "0"
+                }
+            );
+
+            if (Buffer.from(rawBoughtPacks.data.returnData[0], 'base64').toString("hex")) {
+                mintedPacks -= Number("0x" + Buffer.from(rawBoughtPacks.data.returnData[0], 'base64').toString("hex"));
+            }
+
+
+            let rawMintedSkins = await axios.post(`${this.devApi}/query`,
+                {
+                    "scAddress" : this.SCAddressStr,
+                    "funcName"  : "getTotalNFTBuyable",
+                    "args"      : ["01"],
+                    "value"     : "0"
+                }
+            );
+
+            let mintedSkins = Number("0x" + Buffer.from(rawMintedSkins.data.returnData[0], 'base64').toString("hex"));
+
+            let rawBoughtSkins = await axios.post(`${this.devApi}/query`, {
+                    "scAddress" : this.SCAddressStr,
+                    "funcName"  : "getNbrNFTBought",
+                    "args"      : ["01"],
+                    "value"     : "0"
+                }
+            );
+
+            if (Buffer.from(rawBoughtSkins.data.returnData[0], 'base64').toString("hex")) {
+                mintedSkins -= Number("0x" + Buffer.from(rawBoughtSkins.data.returnData[0], 'base64').toString("hex"));
+            }
+
+            if(mintedPacks || mintedSkins) {
+                this.link = '/mint-page'
+            }
+        },
+    },
+    async beforeMount() {
+        await this.checkIfMintIsLive()
+    }
+
+};
+</script>

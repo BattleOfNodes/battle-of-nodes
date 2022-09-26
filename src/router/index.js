abdd41fbb2790763f45372fd51d371d4c172c12a
Andrei-George Caloian <48419225+caloianandreig@users.noreply.github.com>
2022-09-26 23:49:05 +0300

    unused-fix

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import Homepage from '@/pages/Homepage/Homepage'
 import MyCollection from '@/pages/MyCollection/MyCollection'
 import MintPage from '@/pages/MintPage/MintPage'
 import DownloadDemo from '@/pages/DownloadDemo/DownloadDemo'
-import staking from '@/pages/Staking/staking'
+// import staking from '@/pages/Staking/staking'
 import vmodal from 'vue-js-modal'
 
 Vue.use(VueRouter)

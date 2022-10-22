f9edc34c517e60cfd39a7ba98fdd4cffc9341695
Andrei-George Caloian <48419225+caloianandreig@users.noreply.github.com>
2022-10-22 23:59:22 +0300

    fix-main

--- src/pages/MintPage/MintPage.vue
+++ src/pages/MintPage/MintPage.vue
@@ -270,13 +270,13 @@ export default {
 
             selectedWineOption: 1,
 
-            // ELROND //
-            devApi: "https://devnet-api.elrond.com",
-            SCAddressStr: "erd1qqqqqqqqqqqqqpgqyaqtxp57538uzjl6d5krpfrrzquv5f0ctvkqn59qft",
+            // // ELROND //
+            // devApi: "https://devnet-api.elrond.com",
+            // SCAddressStr: "erd1qqqqqqqqqqqqqpgqyaqtxp57538uzjl6d5krpfrrzquv5f0ctvkqn59qft",
 
             // main
-            // devApi : "https://api.elrond.com",
-            // SCAddressStr: "erd1qqqqqqqqqqqqqpgqfj9qht90c9zldjskq62sfx8ugfdxpjte58sq7r8au4",
+            devApi : "https://api.elrond.com",
+            SCAddressStr: "erd1qqqqqqqqqqqqqpgqfj9qht90c9zldjskq62sfx8ugfdxpjte58sq7r8au4",
 
         }
     },
